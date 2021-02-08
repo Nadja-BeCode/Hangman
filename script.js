525d5731f73c528e8915556a091183477d9b716e
Nadja-BeCode <nadja.agostinho@gmail.com>
2021-02-08 15:54:18 +0100

    added but not finished yet

---
+++ script.js
@@ -0,0 +1,15 @@
+let words = ["Aeroplane","Vampire","Craft", "Parfum","Force","Port","Bubble"] // my words hidden list
+
+let maxLivesLeft = 7; // max lives that I gave
+let needToFind = []; // new arr enter words 
+let failure = ?; // how many failure shout I wrote? Do I need to write? Is that necessary?
+
+
+
+function randomWord() { //How will be insert in
+
+}
+
+function alphabetsButtons () {
+    
+}

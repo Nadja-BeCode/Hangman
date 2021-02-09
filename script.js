9de1db475abc98dc0ddc569cb517493daf032909
Nadja-BeCode <nadja.agostinho@gmail.com>
2021-02-09 09:30:52 +0100

    added + modif

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let words = ["Aeroplane","Vampire","Craft", "Parfum","Force","Port","Bubble"] //
 
 let maxLivesLeft = 7; // max lives that I gave
 let needToFind = []; // new arr enter words 
-let failure = ?; // how many failure shout I wrote? Do I need to write? Is that necessary?
+let failure = 0; // how many failure shout I wrote? Do I need to write? Is that necessary? Maybe I need to start to Zero 
 
 
 
-function randomWord() { //How will be insert in
-
+function randomWord() { //How will be insert in ?? Don't know what I was trying to say !!!
+    answerWord = words[Math.floor(Math.random() * words.length)]; //To found one of the words 
 }
 
 function alphabetsButtons () {
-    
+
 }
